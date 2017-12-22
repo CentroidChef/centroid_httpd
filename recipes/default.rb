e51c550a8cf2f3f0f0a2ae96eec160adfb3490d2
Kirt Martin <kirt.martin@centroid.com>
2017-12-22 15:07:20 -0500

    Add firewall rules

--- recipes/default.rb
+++ recipes/default.rb
@@ -16,6 +16,18 @@ template '/etc/selinux/config' do
   source 'selinux.erb'
 end
 
+# Add rule to firewall to accept http traffic
+execute 'httpd_firewall' do
+  command 'firewall-cmd  --permanent --zone public --add-service http'
+  ignore_failure true
+end
+
+# Reload firewall after adding rule
+execute 'reload_firewall' do
+  command 'firewall-cmd --reload'
+  ignore_failure true
+end
+
 # Setup a simple welcome web page
 template '/var/www/html/index.html' do
   source 'index_html.erb'
